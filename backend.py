3bb35f119b099a5838b9feb1cc140bc73c1bbdb5
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-08-30 04:12:45 -0300

    Adicionando uma função a classe FileHandler quer pega o filepath da imagem escolhida

--- backend.py
+++ backend.py
@@ -3,7 +3,11 @@ import flet as ft
 class FileHandler:
     def __init__(self):
         self.filepath = None
-        self.filePicker = ft.FilePicker()
+        self.filePicker = ft.FilePicker(on_result=self.GetFilepath)
+
+    def GetFilepath(self, e:ft.FilePickerResultEvent):
+        self.filepath = e.files[0].path
+        print(f"Caminho do arquivo: {self.filepath}")
 
     def GetImageInDirectory(self):
         self.filePicker.pick_files(dialog_title="Escolha sua imagem.", initial_directory='/', allow_multiple=False, allowed_extensions=["jpeg", "png"])
