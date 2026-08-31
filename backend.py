14d2c3d4816feb7cdef213bba7291401b3e5dbc3
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-08-31 04:07:55 -0300

    Criando uma função que abre o explorador de arquivo, e uma função que pega o filepath da imagem selecionada

--- backend.py
+++ backend.py
@@ -1,5 +1,24 @@
 from tkinter import filedialog
+from PIL import Image
+import customtkinter as ctk
 
 class FileHandler:
     def __init__(self):
-        pass
+        self.filepath = None
+
+    def SearchFile(self) -> None:
+        self.GetFilename(filedialog.askopenfilename(
+            title="Choice you image file.",
+            multiple=False,
+            initialdir="/",
+            filetypes=(
+                ("image file", ".png"),
+                ("image file", ".jpeg"),
+                ("image file", ".bmp")))
+            )
+
+    def GetFilename(self, filepath) -> None:
+        if filepath != "":
+            self.filepath = filepath
+        else:
+            print("No file chosen")
