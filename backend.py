611ba9c7149352b4a8c9fcaf5b5506e9b4461ffb
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-08-31 02:44:43 -0300

    Começando uma nova classe de FileHandler

--- backend.py
+++ backend.py
@@ -1,13 +1,5 @@
-import flet as ft
+from tkinter import filedialog
 
 class FileHandler:
     def __init__(self):
-        self.filepath = None
-        self.filePicker = ft.FilePicker(on_result=self.GetFilepath)
-
-    def GetFilepath(self, e:ft.FilePickerResultEvent):
-        self.filepath = e.files[0].path
-        print(f"Caminho do arquivo: {self.filepath}")
-
-    def GetImageInDirectory(self):
-        self.filePicker.pick_files(dialog_title="Escolha sua imagem.", initial_directory='/', allow_multiple=False, allowed_extensions=["jpeg", "png"])
+        pass
