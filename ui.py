fa7013d7ba48c30fbea950580703692d80baceba
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-08-31 04:06:47 -0300

    Criando um botão para abrir o gerenciador de arquivos

--- ui.py
+++ ui.py
@@ -1,4 +1,7 @@
 import customtkinter as ctk
+from backend import FileHandler
+
+filehandler = FileHandler()
 
 class App(ctk.CTk):
     def __init__(self):
@@ -6,5 +9,5 @@ class App(ctk.CTk):
         self.geometry("600x500")
         self.title("MySticker")
 
-        self.pick_file = ctk.CTkButton(self, text="Hello, world!")
+        self.pick_file = ctk.CTkButton(self, text="Hello, world!", command=filehandler.SearchFile)
         self.pick_file.grid(row=0, column=0)
