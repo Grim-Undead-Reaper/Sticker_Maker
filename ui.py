0038f3d7fba9526cb5f42878dae073a409db0e08
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-08-31 02:41:13 -0300

    Mudando a codebase de Flet para Customtkinter e Tkinter

--- ui.py
+++ ui.py
@@ -1,22 +1,10 @@
-import flet as ft
-from backend import FileHandler
+import customtkinter as ctk
 
-class App:
+class App(ctk.CTk):
     def __init__(self):
-        self.fileHandler = FileHandler()
+        super().__init__()
+        self.geometry("600x500")
+        self.title("MySticker")
 
-        self.LabelTitle = ft.Text(f"Drag and drop you image")
-        self.AddButton = ft.ElevatedButton(text="Create", on_click=self.GetFile)
-
-    def main(self, page: ft.Page):
-        page.title = "Test"
-
-        page.add(self.LabelTitle)
-        page.add(self.AddButton)
-        page.add(self.fileHandler.filePicker)
-    
-    def RunApp(self):
-        ft.app(target=self.main)
-
-    def GetFile(self, e):
-        self.fileHandler.GetImageInDirectory()
+        self.pick_file = ctk.CTkButton(self, text="Hello, world!")
+        self.pick_file.grid(row=0, column=0)
