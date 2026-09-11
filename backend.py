76b9521cd498da9d10927217b6090d91f3f737e7
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-09-11 07:59:56 -0300

    Criando um função display para exibir o filepath, e ajeitando o botão de pegar arquivo, e transformando todos os tipos de imagems permitidas numa lista

--- backend.py
+++ backend.py
@@ -5,20 +5,26 @@ import customtkinter as ctk
 class FileHandler:
     def __init__(self):
         self.filepath = None
+        self.files_allowed = [".png", ".jpeg", ".jpg", ".bmp"]
 
     def SearchFile(self) -> None:
         self.GetFilename(filedialog.askopenfilename(
             title="Choice you image file.",
             multiple=False,
-            initialdir="/",
+            initialdir=r"E:\Pedro\Downloads",
             filetypes=(
-                ("image file", ".png"),
-                ("image file", ".jpeg"),
-                ("image file", ".bmp")))
+                ("image file", self.files_allowed[0]),
+                ("image file", self.files_allowed[1]),
+                ("image file", self.files_allowed[2]),
+                ("image file", self.files_allowed[3])))
             )
 
     def GetFilename(self, filepath) -> None:
         if filepath != "":
             self.filepath = filepath
+            self.DisplayInfo()
         else:
             print("No file chosen")
+
+    def DisplayInfo(self):
+        print(f"\nFilepath{self.filepath}")
